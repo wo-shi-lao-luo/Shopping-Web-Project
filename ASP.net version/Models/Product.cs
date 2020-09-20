2c09013a5c1299cbcf92aac1595b29e2e8faffc6
kyle-luo <tsjlky@gmail.com>
2020-09-20 16:54:09 -0400

    add Product model

---
+++ ASP.net version/Models/Product.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace ASP.net_version.Models
+{
+    public class Product
+    {
+        [JsonPropertyName("productId")]
+        public string Id { get; set; }
+
+        [JsonPropertyName("productName")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("productPrice")]
+        public float Price { get; set; }
+
+        [JsonPropertyName("productTitle")]
+        public string Title { get; set; }
+
+        [JsonPropertyName("productImage")]
+        public string Image { get; set; }
+
+        [JsonPropertyName("productUrl")]
+        public string URL { get; set; }
+
+        [JsonPropertyName("productDescription")]
+        public string Description { get; set; }
+
+        public override string ToString()
+        {
+            return JsonSerializer.Serialize<Product>(this);
+        }
+    }
+}
