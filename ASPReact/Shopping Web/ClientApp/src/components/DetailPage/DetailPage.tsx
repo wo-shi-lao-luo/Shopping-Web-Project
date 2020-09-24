6f551450fbb899480c7ad50466f1245136b9f014
Chuhan Jie <jiechuhan1996@gmail.com>
2020-09-23 20:05:12 -0700

    detail page structure

--- ASPReact/Shopping Web/ClientApp/src/components/DetailPage/DetailPage.tsx
+++ ASPReact/Shopping Web/ClientApp/src/components/DetailPage/DetailPage.tsx
@@ -5,50 +5,24 @@ import classes from './DetailPage.module.css';
 
 import Navbar from '../Navbar/Navbar';
 import Button from '../Button/Button';
-import img from '../../assets/img/1.jpg';
+import Carousel from '../Carousel/Carousel';
 
-const detailPage = () => {
+const detailPage = (props:any) => {
     return (
-        <div className="">
+        <div>
             <Navbar />
-            <div className={`container ${classes.content}`}>
-                <div className="row">
-                    <div className="col-sm">
-                        <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
-                            <ol className="carousel-indicators">
-                                <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
-                                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-                            </ol>
-
-                            <div className="carousel-inner">
-                                <div className="carousel-item active">
-                                    <img className="d-block" src={img} alt="First slide"></img>
-                                </div>
-                                <div className="carousel-item">
-                                    <img className="d-block" src={img} alt="Second slide"></img>
-                                </div>
-                                <div className="carousel-item">
-                                    <img className="d-block" src={img} alt="Third slide"></img>
-                                </div>
-                            </div>
-                            <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                                <span className="sr-only">Previous</span>
-                            </a>
-                            <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                                <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                                <span className="sr-only">Next</span>
-                            </a>
-                        </div>
+            <div className={`container ${classes.outter}`}>
+                <div className={`row ${classes.content}`}>
+                    <div className="col-xs-12 col-md-6">
+                        <Carousel />
                     </div>
-                </div>
-            </div>
-            <div className="col-sm">
-                <h1>Product Title</h1>
-                <h3>description</h3>
-                <p>price</p>
-                <Button btnType="addToCart">Add to cart</Button>
+                    <div className={`col-xs-12 col-md-6 ${classes.info}`}>
+                        <h1 className={classes.title}>Product Title</h1>
+                        <h3 className={classes.description}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</h3>
+                        <p className={classes.price}>$30</p>
+                        <Button btnType="addToCart">Add to cart</Button>
+                    </div>
+                    </div>
             </div>
         </div>
     );
