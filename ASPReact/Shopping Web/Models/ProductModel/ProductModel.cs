efc4eb7ffff7a942a3b6185bc8863b68cf55c847
kyle-luo <tsjlky@gmail.com>
2020-10-25 15:27:05 -0400

    Update ProductModel.cs

--- ASPReact/Shopping Web/Models/ProductModel/ProductModel.cs
+++ ASPReact/Shopping Web/Models/ProductModel/ProductModel.cs
@@ -2,25 +2,18 @@
 {
     public class ProductModel
     {
-        //[JsonPropertyName("productId")]
         public int Id { get; set; }
 
-        //[JsonPropertyName("productName")]
         public string Name { get; set; }
 
-        //[JsonPropertyName("productPrice")]
         public decimal Price { get; set; }
 
-        //[JsonPropertyName("productTitle")]
         public string Title { get; set; }
 
-        //[JsonPropertyName("productImage")]
         public string Image { get; set; }
 
-        //[JsonPropertyName("productUrl")]
         public string Url { get; set; }
 
-        //[JsonPropertyName("productDescription")]
         public string Description { get; set; }
     }
 }
